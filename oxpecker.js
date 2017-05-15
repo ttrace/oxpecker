61ee55562320fb338643e128c7189a9dc9f30388
Taiyo FUJII <taiyoster@gmail.com>
2017-05-16 05:17:37 +0900

    restraint muted status
    restraint muted status.

--- oxpecker.js
+++ oxpecker.js
@@ -5,7 +5,7 @@ var ox_target_language = ox_config.ox_target_language;
 
 var browser_language = (window.navigator.languages && window.navigator.languages[0]).split("-")[0];
 
-var ox_target_contents_list = $(".status__content").not( $(".ox-flagged"));
+var ox_target_contents_list = $(".status__content").not($(".ox-flagged")).not($(".status.muted .status__content"));
 
 var ox_translation_button_template = document.createElement("A");
      ox_translation_button_template.innerHTML = "文/A";
