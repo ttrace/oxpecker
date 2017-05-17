56fdb10807a8569d8d14f44a1c9e12dd32f56c7a
Taiyo FUJII <taiyoster@gmail.com>
2017-05-17 16:25:07 +0900

    translation-anchor separated
    separated making translation-anchor from oxpecker.js

--- oxpecker.js
+++ oxpecker.js
@@ -5,27 +5,31 @@ var ox_config = require('./oxpecker.config.js');
 var endpoint_url = "https://translation.googleapis.com/language/translate/v2?key=" + ox_config.api_key;
 var ox_target_language = ox_config.ox_target_language;
 
-var browser_language = (window.navigator.languages && window.navigator.languages[0]).split("-")[0];
-
-var ox_target_contents_list = $(".status__content").not($(".ox-flagged")).not($(".status.muted .status__content"));
+//var browser_language = (window.navigator.languages && window.navigator.languages[0]).split("-")[0];
 
 var create_button = require('./translation-button.js');
 var ox_translation_button_template = create_button();
 
+var create_translation_anchor = require('./translation-anchor.js');
+
+// initial work
+var ox_target_contents_list = $(".status__content").not($(".ox-flagged")).not($(".status.muted .status__content"));
 ox_target_contents_list.each(function(i){
      var ox_target_content = ox_target_contents_list[i]
      var mysource = ox_target_content.textContent;
      var translated = "init";
-     var ox_translation_button = ox_translation_button_template.cloneNode(true);
+     var translation_button = ox_translation_button_template.cloneNode(true);
 
-     var translation_anchor = document.createElement("DIV");
-          translation_anchor.id = "translation_" + Math.round( Math.random()*1000 );
+//     var translation_anchor = document.createElement("DIV");
+//          translation_anchor.id = "translation_" + Math.round( Math.random()*1000 );
+     var translation_anchor = create_translation_anchor();
      var target_id = translation_anchor.id;
+
      ox_target_content.appendChild(translation_anchor);
      ox_target_content.classList.add("ox-flagged");
 
-     ox_target_content.parentNode.appendChild( ox_translation_button );
-     ox_translation_button.addEventListener('click', function(){
+     ox_target_content.parentNode.appendChild( translation_button );
+     translation_button.addEventListener('click', function(){
           console.log('started');
           $.ajax({
                          type:     "GET",
@@ -37,7 +41,6 @@ ox_target_contents_list.each(function(i){
                     }
                }).then(
                     function(json){
-                         console.log( json );
 //                         console.log( json.data.translations[0].translatedText );
                          $("#"+target_id).after( json.data.translations[0].translatedText );
                     },
