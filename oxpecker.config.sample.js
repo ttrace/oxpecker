ab07dd009e6be3cb621bf924fdadb5d6d235b514
ZerataX <zeratax@firemail.cc>
2017-09-21 09:58:44 +0200

    add yandex api add support for yandex api

--- oxpecker.config.sample.js
+++ oxpecker.config.sample.js
@@ -1,4 +1,6 @@
 module.exports = {
-     api_key: "your_api_key",
+     google_api_key: "your_google_api_key",
+     yandex_api_key: "your_yandex_api_key",
+     engine: "google or yandex",
      ox_target_language: "ja"
 }
